c0a0822eabb3c7808db1ac5e9c10f5cbcd05fecd
Karthik <rkarthikcricfavour@gmail.com>
2023-09-25 11:57:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+function App() {
+  return (
+    <div><Weather /></div>
+  )
+}
+
+export default App
---
+++ src/components/Weather.jsx
@@ -0,0 +1,50 @@
+// src/Weather.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Weather() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const apiKey = '80441dcf6fc12ff7d5941a2d1ee6ad8b';
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error,incorrect data', error);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchWeatherData();
+  };
+
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+      {weatherData && (
+        <div>
+          <h2>Weather in {weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Weather;
